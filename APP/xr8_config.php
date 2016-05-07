a66b4b774db69774a8f63f98d5a171b8386dad08
biohizard <biohizard@gmail.com>
2016-05-07 03:31:32 -0500

    Ok config local

--- APP/xr8_config.php
+++ APP/xr8_config.php
@@ -22,42 +22,19 @@ if (ZONA == "local") {
 
 if (ZONA == "local") {
 	define("BASE_APP","//localhost/server/XR8/MSgym/APP/");
-	}else{
-		define("BASE_APP","http://app.gymh2o.com/");
-		}
+	}else{}
 
 if (ZONA == "local") {
 	define("BASE_API","//localhost/server/XR8/MSgym/APP/");
-	}else{
-		define("BASE_API","http://app.gymh2o.com/");
-		}
+	}else{}
 
 if (ZONA == "local") {
 	define("BASE_CDN","//localhost/server/XR8/MSgym/CDN/");
-	}else{
-		define("BASE_CDN","http://cdn.gymh2o.com/"); 
-		}
+	}else{}
 
 if (ZONADB == "local") {
-
 	define("BASE_HOSTNAME","localhost");
 	define("BASE_USERNAME","root");
 	define("BASE_PASSWORD","");
 	define("BASE_DATEBASE","gymh2o");
-
-        }else if (ZONADB == "INTERNET") {
-        	
-        	// mysql.us.cloudlogin.co 198.23.53.110:3306
-			define("BASE_HOSTNAME","mysql.us.cloudlogin.co");
-			define("BASE_USERNAME","luxza_gymh2o");
-			define("BASE_PASSWORD","12345aeiou");
-			define("BASE_DATEBASE","luxza_gymh2o");
-
-	        }else{
-
-				define("BASE_HOSTNAME","dvrviena.sytes.net");
-				define("BASE_USERNAME","biohizard");
-				define("BASE_PASSWORD","12345aeiou");
-				define("BASE_DATEBASE","gymh2o");
-
-	                }                
+	}else if (ZONADB == "INTERNET") {}else{}                
