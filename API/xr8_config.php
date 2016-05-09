6891cc484bcccfb1752aad0c4364ab48b502bab0
MSgym <gym@mrkingcoco.com>
2016-05-09 10:34:13 -0500

    gym config local
    configuracion local

--- API/xr8_config.php
+++ API/xr8_config.php
@@ -10,7 +10,7 @@
 date_default_timezone_set('America/Mexico_City');
 
 //Local or Internet
-define("ZONA","local");
+define("ZONA","localgym");
 define("ZONADB","local");
 define("TITLE","local");
 
@@ -19,21 +19,30 @@ if (ZONA == "local") {
 	}else{
 		define('INDEX_PAGE','');
 		}
+
 if (ZONA == "local") {
 	define("BASE_URL","//localhost/server/XR8/MSgym/API");
-	}else{}
+	}else{
+		define("BASE_URL","//localhost/server/XR8/MSgym/API");
+		}
 
 if (ZONA == "local") {
 	define("BASE_APP","//localhost/server/XR8/MSgym/APP/");
-	}else{}
+	}else{
+		define("BASE_APP","//localhost/server/XR8/MSgym/APP/");	
+		}
 
 if (ZONA == "local") {
 	define("BASE_API","//localhost/server/XR8/MSgym/API/");
-	}else{}
+	}else{
+		define("BASE_API","//localhost/server/XR8/MSgym/API/");	
+		}
 
 if (ZONA == "local") {
 	define("BASE_CDN","//localhost/server/XR8/MSgym/CDN/");
-	}else{}
+	}else{
+		define("BASE_CDN","//localhost/server/XR8/MSgym/CDN/");
+		}
 
 if (ZONADB == "local") {
 	define("BASE_HOSTNAME","localhost");
--- CDN/js/plugins.js
+++ CDN/js/plugins.js
@@ -22,7 +22,10 @@
         var ZONAAPI  = "LOCAL"
         var ZONAAPP  = "LOCAL"
         var ZONACDN  = "LOCAL"            
-        var ZONABASE = "//localhost/server/XR8/MSgym/"  
+        //var ZONABASE = "//localhost/server/XR8/MSgym/"  
+        var ZONABASE = "//localhost/MaraSport/"  
+
+        
 
 
             //API URL
